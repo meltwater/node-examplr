af8323af94da386cfe19be55e121bdeeb28b1e44
Evan Sosenko <evan.sosenko@meltwater.com>
2018-09-26 12:26:53 -0700

    Format for Standard 12

--- examples/adventure.js
+++ examples/adventure.js
@@ -1,7 +1,7 @@
 const getLevel = () => Math.floor(10 * Math.random())
 
-export default ({minLevel, log}) => async (adventurer = 'Finn', age = 12) => {
-  log.info({adventurer, age}, 'Ready for adventure!')
+export default ({ minLevel, log }) => async (adventurer = 'Finn', age = 12) => {
+  log.info({ adventurer, age }, 'Ready for adventure!')
 
   const level = await new Promise(resolve => {
     setTimeout(() => resolve(getLevel()), 1000)
@@ -9,5 +9,5 @@ export default ({minLevel, log}) => async (adventurer = 'Finn', age = 12) => {
 
   if (level < minLevel) throw new Error('Level too low.')
 
-  return {adventurer, age, level}
+  return { adventurer, age, level }
 }
