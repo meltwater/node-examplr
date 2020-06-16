10330b61628ae461005114ee7e6f84281589d006
Evan Sosenko <evan.sosenko@meltwater.com>
2020-06-15 17:16:52 -0700

    Run format

--- examples/adventure.js
+++ examples/adventure.js
@@ -3,7 +3,7 @@ const getLevel = () => Math.floor(10 * Math.random())
 export default ({ minLevel, log }) => async (adventurer = 'Finn', age = 12) => {
   log.info({ adventurer, age }, 'Ready for adventure!')
 
-  const level = await new Promise(resolve => {
+  const level = await new Promise((resolve) => {
     setTimeout(() => resolve(getLevel()), 1000)
   })
 
