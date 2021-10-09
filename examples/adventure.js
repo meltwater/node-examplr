e2e43d73533ef49c95d372cd8e3008bdaa06be86
Evan Sosenko <razorx@evansosenko.com>
2021-10-09 16:08:23 -0700

    Run format

--- examples/adventure.js
+++ examples/adventure.js
@@ -1,16 +1,14 @@
 const getLevel = () => Math.floor(10 * Math.random())
 
-export default ({ minLevel, reqId, log }) => async (
-  adventurer = 'Finn',
-  age = 12
-) => {
-  log.info({ adventurer, age }, 'Ready for adventure!')
+export default ({ minLevel, reqId, log }) =>
+  async (adventurer = 'Finn', age = 12) => {
+    log.info({ adventurer, age }, 'Ready for adventure!')
 
-  const level = await new Promise((resolve) => {
-    setTimeout(() => resolve(getLevel()), 1000)
-  })
+    const level = await new Promise((resolve) => {
+      setTimeout(() => resolve(getLevel()), 1000)
+    })
 
-  if (level < minLevel) throw new Error('Level too low.')
+    if (level < minLevel) throw new Error('Level too low.')
 
-  return { adventurer, age, level }
-}
+    return { adventurer, age, level }
+  }
